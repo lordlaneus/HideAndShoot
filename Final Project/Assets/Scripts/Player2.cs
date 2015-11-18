97dfd8830f291ac83c77fa2db368146bc75e024a
Samanta <samantas@nyu.edu>
2015-11-18 16:33:24 -0500

    added UI elements

---
+++ Final Project/Assets/Scripts/Player2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += transform.forward * Time.deltaTime * moveSpeed;
+		}
+		
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += -transform.forward * Time.deltaTime * moveSpeed;
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			transform.position += -transform.right * Time.deltaTime * moveSpeed;
+		}
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			transform.position += transform.right * Time.deltaTime * moveSpeed;
+		}
+
+	}
+}
---
+++ Final Project/Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public Slider player1health;
+	public Slider player2health;
+
+	// Use this for initialization
+	void Start () {
+	
+		player1health.value = 99.0f;
+		player2health.value = 99.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// if player loses health, change the value of the sliding handle
+
+		// if a players health is low, change the color to red
+
+		// if a player dies, lose one of the heart images
+
+	}
+
+	void OnTrigerEnter(Collider myTrigger) {
+		// if a player collects a life, restore 20 health
+
+	}
+}
